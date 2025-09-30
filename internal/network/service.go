40dc0c946ebc3459f7dab3f43c09494fa76ea709
Idhibhat Pankam <marginpankam@gmail.com>
2025-09-30 19:27:56 +0700

    network

--- internal/network/service.go
+++ internal/network/service.go
@@ -1,6 +1,8 @@
 package network
 
 import (
+	"context"
+
 	proto "github.com/bookpanda/firecracker-runner-node/proto/network/v1"
 	"go.uber.org/zap"
 )
@@ -18,4 +20,24 @@ func NewService(log *zap.Logger) Service {
 	return &serviceImpl{
 		log: log,
 	}
+}
+
+func (s *serviceImpl) Setup(ctx context.Context, req *proto.SetupNetworkRequest) (*proto.SetupNetworkResponse, error) {
+	taps, err := Setup(int(req.NumVMs))
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.SetupNetworkResponse{
+		Taps: taps,
+	}, nil
+}
+
+func (s *serviceImpl) Cleanup(ctx context.Context, req *proto.CleanupNetworkRequest) (*proto.CleanupNetworkResponse, error) {
+	err := Cleanup(int(req.NumVMs))
+	if err != nil {
+		return nil, err
+	}
+
+	return &proto.CleanupNetworkResponse{}, nil
 }
