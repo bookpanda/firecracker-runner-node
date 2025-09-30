ee2a1dbc16e2a2ca546766211693d378f85508e1
Idhibhat Pankam <marginpankam@gmail.com>
2025-10-01 00:15:41 +0700

    fs

--- internal/vm/service.go
+++ internal/vm/service.go
@@ -1,6 +1,8 @@
 package vm
 
 import (
+	"context"
+
 	proto "github.com/bookpanda/firecracker-runner-node/proto/vm/v1"
 	"go.uber.org/zap"
 )
@@ -18,4 +20,16 @@ func NewService(log *zap.Logger) Service {
 	return &serviceImpl{
 		log: log,
 	}
+}
+
+func (s *serviceImpl) Create(ctx context.Context, req *proto.CreateVmRequest) (*proto.CreateVmResponse, error) {
+	return nil, nil
+}
+
+func (s *serviceImpl) SendCommand(ctx context.Context, req *proto.SendCommandVmRequest) (*proto.SendCommandVmResponse, error) {
+	return nil, nil
+}
+
+func (s *serviceImpl) GetLogs(ctx context.Context, req *proto.GetLogsVmRequest) (*proto.GetLogsVmResponse, error) {
+	return nil, nil
 }
